2348327003f58670c0f4cebf87853882e8cb5a08
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-02-29 16:06:21 +0100

    misc work

--- src/main/resources/static/app/component/job-request-card/job-request-card.js
+++ src/main/resources/static/app/component/job-request-card/job-request-card.js
@@ -3,7 +3,8 @@
 	angular.module('bruno').component('brJobRequestCard', {
 		templateUrl: 'app/component/job-request-card/job-request-card.html',
 		bindings: {
-			request: "="
+			request: "=",
+			userType: '@'
 		},
 		controller: function($mdDialog) {
 			var ctrl = this;
