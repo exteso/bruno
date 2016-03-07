ab9304a9c299912936b90bc0194eb33eb0d00308
Sylvain Jermini <sylvain.jermini@syjer.com>
2016-03-07 17:02:49 +0100

    add menu

--- src/main/resources/static/app/component/toolbar/toolbar.js
+++ src/main/resources/static/app/component/toolbar/toolbar.js
@@ -2,13 +2,20 @@
 	
 	angular.module('bruno').component('brToolbar', {
 		templateUrl: 'app/component/toolbar/toolbar.html',
-		controller: function(User, $state) {
+		controller: function(User, $state, $window) {
 			var ctrl = this;
+			
+			ctrl.$state = $state;
+			
 			User.currentCachedUser().then(function(user) {
 				ctrl.user = user;
 			}, function() { //error
 				$state.go('login');
 			});
+			
+			ctrl.logout = function() {
+				$window.location.href = "/logout";
+			}
 		}
 	});
 	
